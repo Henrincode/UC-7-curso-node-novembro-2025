9dc1ce201347d26ca337d5b5f97139c5023c2b24
Henrincode <spellctro@gmail.com>
2025-11-11 22:21:24 -0300

    feat: Aprendendo a req e res e usando get e post para mostrar e criar dados via API

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from 'express'
 const app = express()
-app.get('/', (req, res) => {
-    res.send('Hello World')
+app.use(express.json());
+
+const herois = ["Mulher Maravilha", "Super Man", "Homem de Ferro"]
+
+app.get('/herois', (req, res) => {
+    res.send(herois)
 })
-app.get('/oi', (req, res) => {
-    res.send('Olá, mundo!')
+
+app.get('/herois/:id', (req, res) => {
+    const id = req.params.id
+    res.send(herois[id - 1])
 })
+
+app.post('/inserir', (req, res) => {
+  const heroi = req.body.nome
+  herois.push(heroi);
+  res.send(heroi)
+});
 
 app.listen(3000)
