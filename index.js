1a4a8c6c6c26325a2f17b76b9d84d9fa46f8974c
Henrincode <spellctro@gmail.com>
2025-11-25 21:21:54 -0300

    feat: adaptando rotas do rest

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ app.use(express.json());
 
 const herois = ["Mulher Maravilha", "Super Man", "Homem de Ferro"]
 
-app.get('/herois', (req, res) => {
-    res.send(herois)
+app.get('/marvel', (req, res) => {
+  res.send(herois)
 })
 
-app.get('/herois/:id', (req, res) => {
-    const id = req.params.id
-    res.send(herois[id - 1])
+app.get('/marvel/:id', (req, res) => {
+  const id = req.params.id
+  res.send(herois[id - 1])
 })
 
-app.post('/inserir', (req, res) => {
+app.post('/marvel', (req, res) => {
   const heroi = req.body.nome
-  herois.push(heroi);
+  herois.push(heroi)
   res.send(heroi)
 });
 
